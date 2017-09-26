cf57f09997cff1f79a160b27cf741a3b0f133d2b
Juan David <juandavid@Administrators-MacBook-Pro.local>
2017-09-26 12:43:16 -0400

    commit

--- challenge2/index.php
+++ challenge2/index.php
@@ -6,32 +6,31 @@
 </head>
 <body> 
 <h1> Challenge 2 </h1>
-<form method="POST" action="index.php">
- <label for="colors">Search for products</label><input id="color" name="color" type="text" />
+<form method="GET" action="index.php">
+ <label for="color">Search for products</label>
  <select name="color" id="colors">
- <option value="red">RED </option>
- <option value="yellow">Yellow </option>
- <option value="brown">Brown </option>
- <option value="purple"> Purple </option>
+    <option value="red">Red </option>
+    <option value="yellow">Yellow </option>
+    <option value="brown">Brown </option>
+    <option value="purple"> Purple </option>
  </select>
 <input type="submit" value="Submit">
 </form>
 <div>
 <?php
-if(!empty($_POST)){
-    var_dump($_POST);
+if(!empty($_GET) && $_GET["color"] != ""){
     $db = new PDO("mysql:host=localhost;dbname=jdavid_JHernandez;port=8888","r2hstudent", "SbFaGzNgGIE8kfP");
     try {
         //this queries what we actually need 
-        $query = "SELECT name, description, price, color, id FROM Challenge2 WHERE color = :color";
+        $query = "SELECT id, name, description, price, color FROM Challenge2 WHERE color = :color";
         // this gets your statement ready
         $prepared = $db->prepare($query);
-        $prepared->execute(array(':color' => $_POST["color"]));
+        $prepared->execute(array(':color' => $_GET["color"]));
         $items = $prepared->fetchAll();
         print_r($items);
 
         foreach( $items as $item){
-            echo "<p>{$color["name"]}, {$color["color"]}</p>";
+            echo "<p>{$item["name"]}, {$item["color"]}</p>";
         }
     } catch (Exception $e) {
         echo "Bad query";
