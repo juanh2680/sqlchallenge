defada32aad9fcab56a67faf1724eb5cddec7443
Juan David <juandavid@Administrators-MacBook-Pro.local>
2017-09-26 13:15:45 -0400

    adding whitespace

--- challenge3/index.php
+++ challenge3/index.php
@@ -25,7 +25,7 @@ if(!empty($_POST)){
            ':name' => $_POST["name"],
            ':description' => $_POST["description"], 
            ':price' => $_POST["price"], 
-           ':color' => $_POST["color"],  
+           ':color' => $_POST["color"],
        ));
        print_r($b);
     } catch (Exception $e) {
