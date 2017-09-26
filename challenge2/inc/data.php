9e87539934aa108d60ffce86953dbc32eec79d73
Juan David <juandavid@Administrators-MacBook-Pro.local>
2017-09-26 10:31:01 -0400

    new try

---
+++ challenge2/inc/data.php
@@ -0,0 +1,23 @@
+<?php 
+if(!empty($_GET)){
+$db = new PDO("mysql:host=localhost;dbname=JHernandex;port=8888","r2hstudent", "SbFaGzNgGIE8kfP");
+try {
+    //this queries what we actually need 
+   $results = $db->query("SELECT name,description,price,color,id FROM Challenge2 WHERE color= :colors");
+  // this gets your statement ready
+   $prepared = $db->prepare($results);
+
+   
+   $prepared->bindParam(':colors',$_GET["colors"]);
+   $prepared->execute();
+
+   foreach($prepared->fetchAll() as $color){
+       echo "<p>{$color["name"]}, {$color["color"]}</p>";
+   }
+} catch (Exception $e) {
+    echo "Bad query";
+    exit;
+}
+}
+$array =$results->fetchAll(PDO::FETCH_ASSOC);
+?>
---
+++ challenge2/index.php
@@ -0,0 +1,22 @@
+<?php include("inc/data.php"); ?> 
+<!DOCTYPE html> 
+<html>
+<head>
+<title> 
+</title> 
+</head>
+<body> 
+<h1> Challenge 2 </h1>
+<form method="GET">
+ <label for="colors">Search for products</label><input id="color" name="color" type="text" />
+ <select name="colors" id="colors">
+ <option value="red">RED </option>
+ <option value="yellow">Yellow </option>
+ <option value="brown">Brown </option>
+ <option value="purple"> Purple </option>
+ </select>
+<input type="submit" value="Submit">
+
+</form>
+</body>
+</html>
